b8d520b821e9142371fa6900cd48dafed42e1a49
ghaust <joseph.banzio@contentsquare.com>
2023-08-27 04:17:19 +0200

    feat: setup mysql db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
+
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+}
+
+func openDB(datasource string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", datasource)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func main() {
@@ -20,12 +39,25 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile|log.LUTC)
 
+	err := godotenv.Load("./.env")
+
+	if err != nil {
+		errorLog.Fatal("Error loading .env file")
+	}
+
+	db, err := openDB(os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	
 	infoLog.Printf("Starting server on %s", *addr)
 
 	server := &http.Server{
@@ -34,6 +66,6 @@ func main() {
 		Handler:  app.routes(),
 	}
 	// requests are handled concurrently, all incoming requests are served in their own goroutine = higher risk of race conditions when accessing shared resources
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
 }
